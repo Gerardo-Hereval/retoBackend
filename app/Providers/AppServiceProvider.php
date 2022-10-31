d32804706ce3deff4930cf807cd444e18d545bd3
“Carlos <carlos.valenzh@gmail.com>
2022-10-31 03:01:17 -0600

    Prueba

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->environment('production'))
+        if ($this->app->environmen('production'))
         {
             URL::forceScheme('https');
 
