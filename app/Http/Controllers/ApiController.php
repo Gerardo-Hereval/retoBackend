c373550ec46daa0c0254ed2f3db7bda563a3cfd6
“Carlos <carlos.valenzh@gmail.com>
2022-11-01 07:21:40 -0600

    Prueba con varios resultados

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 //modelos a ocupar
 use App\Models\Zip_code;
 
+
+
 class ApiController extends Controller
 { //funcion para hacer el llamado a la base de datos mediante Eloquents
      public function index($zc){
@@ -14,8 +16,8 @@ class ApiController extends Controller
             //lo ingresamos a un for por los codigos postales que tienen más asentamientos
             for ($i = 0,$long=count($zip_code);$i<$long;++$i){
                 //obtenemos la informacion de la base de datos y empezamos a organizar el json mediante arrays
-                $data[$i] = array("zip_code"=>$zip_code[$i]->zip_code,
-                    "locality"=>$zip_code[$i]->ciudad,
+                $data[0] = array("zip_code"=>$zip_code[0]->zip_code,
+                    "locality"=>$zip_code[0]->ciudad,
                     "federal_entity"=>array("key"=>$zip_code[$i]->clave_entidad,
                         "name"=>$zip_code[$i]->entidad,
                         "code"=>null),
@@ -23,7 +25,7 @@ class ApiController extends Controller
                         "name"=>$zip_code[$i]->asentamiento,
                         "zona_type"=>$zip_code[$i]->zona_ubi_asent,
                         "senttlement_type"=>array("name"=>$zip_code[$i]->tipo_asentamiento)),
-                    "municipality"=>array("key"=>$zip_code[$i]->clave_municipio,"name"=>$zip_code[$i]->municipio));
+                    "municipality"=>array("key"=>$zip_code[0]->clave_municipio,"name"=>$zip_code[0]->municipio));
                 //mandamos como reponse el json de la información obtenida de la base de datos
             }
             return response()->json($data);
