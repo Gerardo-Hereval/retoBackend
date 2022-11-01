fe8b50d2a4c76ba7d00e365b89a5e4fed57c0b04
“Carlos <carlos.valenzh@gmail.com>
2022-10-31 20:08:20 -0600

    Mod de Controllador de Api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,19 +9,19 @@ class ApiController extends Controller
 
 {
     //funcion para hacer el llamado a la base de datos mediante eloquents
-    public function zip_codes(Zip_code $zip_code){
+    public function zip_codes(Zip_code $zc){
         //obtenemos la informacion de la base de datos y empezamos a organizar el json mediante arrays
 
-        $data = array("zip_code"=>$zip_code['zip_code'],
-            "locality"=>$zip_code['ciudad'],
-            "federal_entity"=>array("key"=>$zip_code['clave_entidad'],
-                                    "name"=>$zip_code['entidad'],
+        $data = array("zip_code"=>$zc['zip_code'],
+            "locality"=>$zc['ciudad'],
+            "federal_entity"=>array("key"=>$zc['clave_entidad'],
+                                    "name"=>$zc['entidad'],
                                     "code"=>null),
-            "settlements"=>array("key"=>$zip_code['identificador_uni_asent'],
-                                "name"=>$zip_code['asentamiento'],
-                                "zona_type"=>$zip_code['zona_ubi_asent'],
-                                "senttlement_type"=>array("name"=>$zip_code['tipo_asentamiento'])),
-            "municipality"=>array("key"=>$zip_code['clave_municipio'],"name"=>$zip_code['municipio']));
+            "settlements"=>array("key"=>$zc['identificador_uni_asent'],
+                                "name"=>$zc['asentamiento'],
+                                "zona_type"=>$zc['zona_ubi_asent'],
+                                "senttlement_type"=>array("name"=>$zc['tipo_asentamiento'])),
+            "municipality"=>array("key"=>$zc['clave_municipio'],"name"=>$zc['municipio']));
 
         //mandamos como reponse el json de la información obtenida de la base de datos
         return response()->json($data);
