269e58c3de169523afa7265578f2e0d11f09f421
“Carlos <carlos.valenzh@gmail.com>
2022-11-01 07:43:32 -0600

    Prueba con varios resultados3

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,7 +16,10 @@ class ApiController extends Controller
             //lo ingresamos a un for por los codigos postales que tienen más asentamientos
                 //obtenemos la informacion de la base de datos y empezamos a organizar el json mediante arrays
                 $data[0] = array("zip_code"=>$zip_code[0]->zip_code,
-                    "locality"=>$zip_code[0]->ciudad);
+                    "locality"=>$zip_code[0]->ciudad,
+                    "federal_entity"=>array("key"=>$zip_code[0]->clave_entidad,
+                        "name"=>$zip_code[0]->entidad,
+                        "code"=>null));
                 for ($i = 0,$long=count($zip_code);$i<$long;++$i){
                     $data1[0]=array(
                         "federal_entity"=>array("key"=>$zip_code[$i]->clave_entidad,
@@ -28,7 +31,7 @@ class ApiController extends Controller
                             "senttlement_type"=>array("name"=>$zip_code[$i]->tipo_asentamiento)));}
             $data2[0]=array("municipality"=>array("key"=>$zip_code[0]->clave_municipio,"name"=>$zip_code[0]->municipio));
                 //mandamos como reponse el json de la información obtenida de la base de datos
-            $datas=[$data,$data1,$data2];
+            $datas=array_merge($data,$data1,$data2);
             return response()->json($datas);
         }
 
