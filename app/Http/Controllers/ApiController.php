4b84428b8a4359223eb30474118f0c8da3c45821
“Carlos <carlos.valenzh@gmail.com>
2022-11-01 08:06:13 -0600

    Prueba con varios resultados7

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,17 +16,16 @@ class ApiController extends Controller
             //lo ingresamos a un for por los codigos postales que tienen más asentamientos
                 //obtenemos la informacion de la base de datos y empezamos a organizar el json mediante arrays
             for ($i = 0,$long=count($zip_code);$i<$long;++$i){
-                $data[0] = array("zip_code"=>$zip_code[0]->zip_code,
+                $data[$i] = array("zip_code"=>$zip_code[0]->zip_code,
                     "locality"=>$zip_code[0]->ciudad,
                     "federal_entity"=>array("key"=>$zip_code[0]->clave_entidad,
                         "name"=>$zip_code[0]->entidad,
                         "code"=>null),
-                    array(
-                        "settlements"=>array("key"=>$zip_code[$i]->identificador_uni_asent,
+                    array("settlements"=>array("key"=>$zip_code[$i]->identificador_uni_asent,
                             "name"=>$zip_code[$i]->asentamiento,
                             "zona_type"=>$zip_code[$i]->zona_ubi_asent,
                             "senttlement_type"=>array("name"=>$zip_code[$i]->tipo_asentamiento))),
-            array("municipality"=>array("key"=>$zip_code[0]->clave_municipio,"name"=>$zip_code[0]->municipio)));
+                    array("municipality"=>array("key"=>$zip_code[0]->clave_municipio,"name"=>$zip_code[0]->municipio)));
                 return response()->json($data[$i]);
             }
                 //mandamos como reponse el json de la información obtenida de la base de dato
