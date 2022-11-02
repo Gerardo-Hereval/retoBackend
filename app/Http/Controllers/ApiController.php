663460a7521e6af4d083831e2aaa473ef5fffe32
“Carlos <carlos.valenzh@gmail.com>
2022-11-01 18:36:47 -0600

    Array dentro de un array

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,36 +3,40 @@
 namespace App\Http\Controllers;
 
 //modelos a ocupar
+use App\Models\Federal_entity;
+use App\Models\Municipality;
+use App\Models\Settlement;
 use App\Models\Zip_code;
 
 
-
 class ApiController extends Controller
-{ //funcion para hacer el llamado a la base de datos mediante Eloquents
-     public function index($zc){
+{
+    public function index($zc)
+    {
         //realizamos la busqueda de información
-        $zip_code=Zip_code::where('zip_code',$zc)->get();
-        if(isset($zip_code)){
-            //lo ingresamos a un for por los codigos postales que tienen más asentamientos
-                //obtenemos la informacion de la base de datos y empezamos a organizar el json mediante arrays
-            for ($i = 0,$long=count($zip_code);$i<$long;++$i){
-                $data[$i] =
-                    array("zip_code"=>$zip_code[$i]->zip_code,
-                        "locality"=>$zip_code[$i]->ciudad,
-                        "federal_entity"=>array("key"=>$zip_code[$i]->clave_entidad,
-                        "name"=>$zip_code[$i]->entidad,
-                        "code"=>null),
-                    array("settlements"=>array("key"=>$zip_code[$i]->identificador_uni_asent,
-                        "name"=>$zip_code[$i]->asentamiento,
-                        "zona_type"=>$zip_code[$i]->zona_ubi_asent,
-                        "senttlement_type"=>array("name"=>$zip_code[$i]->tipo_asentamiento))),
-                    array("municipality"=>array("key"=>$zip_code[$i]->clave_municipio,"name"=>$zip_code[$i]->municipio)));
-                return response()->json($data[$i]);
-            }
-                //mandamos como reponse el json de la información obtenida de la base de dato
+        $zip_code = Zip_code::where('zip_code', $zc)->get();
+        //lo ingresamos a un for por los codigos postales que tienen más asentamientos
+        $data= array("zip_code" => $zip_code[0]->zip_code,
+            "locality" => $zip_code[0]->ciudad,
+            "federal_entity" => array("key" => $zip_code[0]->clave_entidad,
+                "name" => $zip_code[0]->entidad,
+                "code" => null));
+        for ($i = 0, $long = count($zip_code); $i < $long; ++$i) {
+            //obtenemos la informacion de la base de datos y empezamos a organizar el json mediante arrays
 
-        }
-
+            $data1[$i] = array("settlements" => array("key" => $zip_code[$i]->identificador_uni_asent,
+                "name" => $zip_code[$i]->asentamiento,
+                "zona_type" => $zip_code[$i]->zona_ubi_asent,
+                "senttlement_type" => array("name" => $zip_code[$i]->tipo_asentamiento)));
+        };
+        $data2=array("municipality" => array("key" => $zip_code[0]->clave_municipio, "name" => $zip_code[0]->municipio));
+            //mandamos como reponse el json de la información obtenida de la base de datos
+        //$datas1= array_slice($data1,0);
+        $datas = $data + $data1 + $data2;
+        //dd(gettype($datas));//es un array
+        //array_values(array_values($datas1))
+        //dd($datas);
+        return response()->json($datas);
     }
-
+    //mandamos como reponse el json de la información obtenida de la base de dato
 }
---
+++ database/migrations/2022_11_01_214333_create_settlement_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settlements', function (Blueprint $table) {
+            $table->string('zip_code');
+            $table->string('id_asenta_cpcons');
+            $table->string('d_asenta');
+            $table->string('d_zona');
+            $table->string('settlement_type');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settlements');
+    }
+};
