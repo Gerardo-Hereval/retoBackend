7128f52a0260f8e9a99095e1f0a46c6f400e8114
“Carlos <carlos.valenzh@gmail.com>
2022-11-01 19:10:09 -0600

    Orden de arrays

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 //modelos a ocupar
-use App\Models\Federal_entity;
-use App\Models\Municipality;
-use App\Models\Settlement;
 use App\Models\Zip_code;
 
 
@@ -15,27 +12,23 @@ class ApiController extends Controller
     {
         //realizamos la busqueda de información
         $zip_code = Zip_code::where('zip_code', $zc)->get();
-        //lo ingresamos a un for por los codigos postales que tienen más asentamientos
+        //obtenemos la informacion de la base de datos y empezamos a organizar el json mediante arrays
         $data= array("zip_code" => $zip_code[0]->zip_code,
             "locality" => $zip_code[0]->ciudad,
             "federal_entity" => array("key" => $zip_code[0]->clave_entidad,
                 "name" => $zip_code[0]->entidad,
                 "code" => null));
+        //lo ingresamos a un for por los codigos postales que tienen más asentamientos
         for ($i = 0, $long = count($zip_code); $i < $long; ++$i) {
-            //obtenemos la informacion de la base de datos y empezamos a organizar el json mediante arrays
-
-            $data1[$i] = array("settlements" => array("key" => $zip_code[$i]->identificador_uni_asent,
+            $data1[$i] = array("key" => $zip_code[$i]->identificador_uni_asent,
                 "name" => $zip_code[$i]->asentamiento,
                 "zona_type" => $zip_code[$i]->zona_ubi_asent,
-                "senttlement_type" => array("name" => $zip_code[$i]->tipo_asentamiento)));
+                "senttlement_type" => array("name" => $zip_code[$i]->tipo_asentamiento));
         };
+        //continuamos obteniendo la data en otro array
         $data2=array("municipality" => array("key" => $zip_code[0]->clave_municipio, "name" => $zip_code[0]->municipio));
-            //mandamos como reponse el json de la información obtenida de la base de datos
-        //$datas1= array_slice($data1,0);
-        $datas = $data + $data1 + $data2;
-        //dd(gettype($datas));//es un array
-        //array_values(array_values($datas1))
-        //dd($datas);
+        //creamos una variable con las arrays para imprimirlo
+        $datas = $data + array("settlements"=>$data1) + $data2;
         return response()->json($datas);
     }
     //mandamos como reponse el json de la información obtenida de la base de dato
